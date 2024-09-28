694581c9b42a4864a834fab58d1ed0aa6a8bd795
AlexJelani <jelanialexander82@gmail.com>
2024-09-28 00:04:16 +0000

    added new bucket tf code

--- new_bucket.tf
+++ new_bucket.tf
@@ -1,12 +1,19 @@
-# Specify the provider and access details
 
+# Specify the provider and region here
 provider "aws" {
-  
+  region = "us-east-1"  # Replace with your desired region
+}
+
+# Generate a random suffix for the S3 bucket name to ensure uniqueness
+resource "random_string" "bucket_suffix" {
+  length  = 6
+  special = false
+  upper   = false
 }
 
 # Create an S3 bucket
 resource "aws_s3_bucket" "udabucket" {
-  bucket = "cicd-terraform-demo-bucket20213"
+  bucket = "cicd-terraform-demo-bucket-${random_string.bucket_suffix.result}"
 
   tags = {
     Name        = "CICD test bucket"
@@ -14,9 +21,20 @@ resource "aws_s3_bucket" "udabucket" {
   }
 }
 
+# Lookup the latest Amazon Linux 2 AMI
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 # Create an EC2 instance
 resource "aws_instance" "ec2_instance" {
-  ami           = "ami-04a0ae173da5807d3"
+  ami           = data.aws_ami.latest_amazon_linux.id
   instance_type = "t2.micro"
 
   tags = {
